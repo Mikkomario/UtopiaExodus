0c7a593c5a4505857233f97264fd037815ad24a8
Mikkomario <mikko_hilpinen@hotmail.com>
2014-12-03 14:38:45 +0200

    The initial commit. Moved the files from Omega and reorganized them.

---
+++ src/exodus_util/ExodusResourceType.java
@@ -0,0 +1,18 @@
+package exodus_util;
+
+import arc_resource.ResourceType;
+
+/**
+ * The different types of resources introduced in the Exodus module
+ * 
+ * @author Mikko Hilpinen
+ * @since 2.12.2014
+ */
+public enum ExodusResourceType implements ResourceType
+{
+	/**
+	 * Areas contain different GameObjects. Areas should not be introduced in a GamePhase 
+	 * since they control GamePhases and require them in order to work.
+	 */
+	AREA;
+}
